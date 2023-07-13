a30b837b8b13e64f4db04a991eb897be20ac4453
Akhmed Valiaiev <4bselyam@gmail.com>
2023-07-13 15:55:38 +0200

    devsh changes

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ echo "Switching on master"
 git checkout master
 
 echo "Build app on poty 80"
-npm run build
+npm run deploy
 
 echo "Deploying to server"
 scp -r build/* wld@212.227.166.5:/var/www/212.227.166.5/
