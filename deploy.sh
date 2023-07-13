14e13ec2e0beadeadf9bfe0db89eaa7d0c94bc6e
Akhmed Valiaiev <4bselyam@gmail.com>
2023-07-13 15:27:56 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Build app on poty 80"
 npm run build
 
 echo "Deploying to server"
-scp -r build/* root@${212.227.166.5}:/var/www/212.227.166.5/
+scp -r build/* wld@212.227.166.5:/var/www/212.227.166.5/
 
 echo "Deployed successfully"
