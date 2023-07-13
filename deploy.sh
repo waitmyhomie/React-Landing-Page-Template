8891132c30e5d2bb425adb8bbbff2740f433ea87
Akhmed Valiaiev <4bselyam@gmail.com>
2023-07-13 18:29:46 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ echo "Switching on master"
 git checkout master
 
 echo "Deploying to server"
-scp -r build/* wld@212.227.166.5:/var/www/212.227.166.5/
+scp -r build/* wld@212.227.166.5:/var/www/batiste-sp.de/
 
 echo "Deployed successfully"
